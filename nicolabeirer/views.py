eb3b7919079db5830bba5a98d312ad5fa36c8fbb
Nicola Beirer <nicolabeirer12@gmail.com>
2026-02-27 15:27:30 -0800

    update home page

--- nicolabeirer/views.py
+++ nicolabeirer/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from blog.models import BlogPage
+from adventures.models import AdventurePage
+from projects.models import ResumeProject
+
 
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'recent_posts': BlogPage.objects.live().order_by('-date')[:3],
+        'recent_adventures': AdventurePage.objects.live().order_by('-date_start')[:3],
+        'recent_projects': ResumeProject.objects.all()[:3],
+    }
+    return render(request, 'home.html', context)
